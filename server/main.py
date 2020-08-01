402d4858c0789d9813798d35c7eb3def0662f7ce
Ashu Sharma <ashu7073657271@gmail.com>
2020-08-01 15:23:19 +0530

    Create a Base Model for latitude and longitude values

--- server/main.py
+++ server/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pysolar.solar import *
+from pydantic import BaseModel
 
 # Instance of FastAPI class
 app = FastAPI()
+
+
+class SunModel(BaseModel):
+    latitude
+    float
+    longitude: float
 
 
 @app.post("/")
